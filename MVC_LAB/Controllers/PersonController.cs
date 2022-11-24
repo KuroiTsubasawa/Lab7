aec6906d718df2b20f821cf90be08fb0ed369b61
Nazarii Borsulia <w65431@student.wsiz.edu.pl>
2022-11-24 13:13:42 +0100

    init

---
+++ MVC_LAB/Controllers/PersonController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_LAB.Models.Person;
+
+namespace MVC_LAB.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new PersonViewModel()
+            {
+                Person = new PersonModel().GetPersons()
+            };
+            return View(model);
+        }
+    }
+    
+}
